f06daf59d124252809ff29210bc3e68a69b82d27
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-04-22 03:12:23 -0600

    Created Program for Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Clear();
+        Console.Write("What is your Grade percentage? ");
+        string gradeNumber = Console.ReadLine();
+
+        int gradePercentage = int.Parse(gradeNumber);
+
+        string  gradeLetter = "";
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+         else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+         else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {gradeLetter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, You have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did not pass, Better Luck next time.");
+        }
+
+
     }
 }
