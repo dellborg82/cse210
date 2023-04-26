3cee52f5a03f5ed57964b4512f89bfe07e9755c3
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-04-25 21:21:41 -0600

    Created Preperation 3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Clear Screen
+        Console.Clear();
+
+        // Main Code
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        int numberGuess = -1;
+        int numOfGuesses = 0;
+
+        // Loop
+        while (numberGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine());  
+
+            if (magicNumber > numberGuess)
+            {
+                Console.WriteLine("Higher");
+                numOfGuesses = numOfGuesses +1;
+            }
+            else if (magicNumber < numberGuess)
+            {
+                Console.WriteLine("Lower");
+                numOfGuesses = numOfGuesses +1;
+            }
+            else
+            {
+                Console.WriteLine($"You Guessed it! It took you {numOfGuesses} guesses!");
+            }
+        }
     }
 }
