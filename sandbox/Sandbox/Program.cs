1070085023b2190a29839d898afe535c46f4ec3e
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-05-09 05:01:30 -0600

    updated class program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,68 @@
 using System;
 
+
+// Class: Person (Name of the Class)
+// Attributes: (Member Variables)
+// * _givenName : string
+// * _familyName : string
+
+// Behaviors: (Methods or Functions)
+// * ShowEasternName() : void
+// * ShowWesternName() : void
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Clear Screen
+        Console.Clear();
+
+        // Prompt for the Class name and stor it in a variable
+        Console.Write("Please enter the Class Name: ");
+        string className = Console.ReadLine();
+        
+        // Create Lists for Attributes and Behaviors
+        List<string> attributes = new List<string>();
+        Console.WriteLine("Enter the Class Attributes (Member Variables). Type done when complete.");
+        string attribute = Console.ReadLine();
+        while (attribute != "done")
+        {
+            attributes.Add(attribute);
+            attribute = Console.ReadLine();
+        }
+
+        List<string> behaviors = new List<string>();
+        Console.WriteLine("Enter the Class Behaviors (Methods or Functions). Type done when complete");
+        string behavior = Console.ReadLine();
+        while (behavior != "done")
+        {
+            behaviors.Add(behavior);
+            behavior = Console.ReadLine();
+        }
+
+        // Display the Class Diagram
+        Console.WriteLine($"Bellow you will find the Class Diagram for {className}:");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("|                                      |");
+        Console.WriteLine($"|            {className}              |");
+        Console.WriteLine("|                                      |");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("|                                      |");
+
+        // Print attributes
+        foreach (string attributeItem in attributes)
+        {
+            Console.WriteLine($"|            {attributeItem}              |");
+        }
+        Console.WriteLine("|                                      |");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("|                                      |");
+         foreach (string behaviorItem in behaviors)
+        {
+            Console.WriteLine($"|            {behaviorItem}              |");
+        }
+        Console.WriteLine("|                                      |");
+        Console.WriteLine("----------------------------------------");
     }
 }
