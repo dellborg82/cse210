b493266a2a56df18a774ab2f6d049587fe51c4f8
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-05-13 11:54:42 -0600

    Added comments to Prompt Generator

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -3,20 +3,22 @@ using System;
 public class PromptGenerator
 
 {
-    public string displayPrompt;
+    public string _displayPrompt;
     public void randomPrompt()
     {
+        // Create a list of Prompts for the program
         List<string> listOfPrompts = new List<string>();
         listOfPrompts.Add("What was the BEST part of my day today? ");
         listOfPrompts.Add("What was the WORST part of my day today? ");
-        listOfPrompts.Add("How did I apply the messages of the Matilday Movie in my day today? ");
+        listOfPrompts.Add("How did I apply the messages of the Matilda Movie in my day today? ");
         listOfPrompts.Add("How did I see the hand of the Lord in my life today? ");
         listOfPrompts.Add("If I could do one thing that I did today over, What would it be, and why? ");
         listOfPrompts.Add("What goals did I work on today? How did I work them them? ");
 
+        // Randomze the prompt
         Random random = new Random();
         int index = random.Next(listOfPrompts.Count);
-        displayPrompt = listOfPrompts[index];
-        Console.WriteLine(displayPrompt);
+        _displayPrompt = listOfPrompts[index];
+        Console.WriteLine(_displayPrompt);
     }
 }
