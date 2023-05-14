1ce48670bcc69cc40772070c5e8b47b9ef163be9
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-05-13 20:23:47 -0600

    added questiond

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -12,6 +12,9 @@ public class Prompt
         listOfPrompts.Add("How did I see the hand of the Lord in my life today? ");
         listOfPrompts.Add("If I could do one thing that I did today over, What would it be, and why? ");
         listOfPrompts.Add("What goals did I work on today? How did I work them them? ");
+        listOfPrompts.Add("What was your favorite thing that you said today, and Why? ");
+        listOfPrompts.Add("The Day is over, What TV Show do you watch, and why? ");
+        listOfPrompts.Add("If you went to hogwarts and the sorting hat say's Slitherin to you how do tell him that you think that he's wrong? ");
         
         Random random = new Random();
         int index = random.Next(listOfPrompts.Count);
