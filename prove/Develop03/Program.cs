65e5715885e592ddfcd9fc98f64411286af2a25d
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-05-27 23:57:37 -0600

    udpated Scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,8 +8,8 @@ class Program
         List<Scripture> scriptures = new List<Scripture>();
 
         scriptures.Add(new Scripture(new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."));
-        scriptures.Add(new Scripture(new Reference("1 Nephi", 3, 7), "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no bcommandments unto the children of men, save he shall cprepare a way for them that they may accomplish the thing which he commandeth them."));
-        scriptures.Add(new Scripture(new Reference("Romans", 8, 16, 17), "The Spirit itself beareth witness with our spirit, that we are the children of God: And if children, then heirs; heirs of God, and joint-heirs with Christ; if so be that we suffer with him, that we may be also glorified together."));
+        scriptures.Add(new Scripture(new Reference("Enos", 1, 13-14, 16), "And now behold, this was the desire which I desired of him—that if it should so be, that my people, the Nephites, should fall into transgression, and by any means be destroyed, and the Lamanites should not be destroyed, that the Lord God would preserve a record of my people, the Nephites; even if it so be by the power of his holy arm, that it might be brought forth at some future day unto the Lamanites, that, perhaps, they might be brought unto salvation— 14 For at the present our strugglings were vain in restoring them to the true faith. And they swore in their wrath that, if it were possible, they would destroy our records and us, and also all the traditions of our fathers. 16 And I had faith, and I did cry unto God that he would preserve the records; and he covenanted with me that he would bring them forth unto the Lamanites in his own due time."));
+        scriptures.Add(new Scripture(new Reference("3 Nephi", 11, 7), "Behold my Beloved Son, in whom I am well pleased, in whom I have glorified my name—hear ye him."));
 
         Random random = new Random();
         int randomScripture = random.Next(0, scriptures.Count);
