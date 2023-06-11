e0fa0f95d4a00e470a49940c2d86e72d4d834296
dellborg82 <76897447+dellborg82@users.noreply.github.com>
2023-06-10 20:55:00 -0600

    .

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,47 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    private string[] _reflectivePrompts = {
+        "How did you apply the teachings of the gospel in your life today?",
+        "How did the Matilda Musical affect you today?",
+        "How did you work on your Marriage today?",
+        "When have you felt the Holy Ghost this month?",
+        "What good dead(s) did you perform today?"
+    };
+
+    public ListingActivity()
+    {
+        SetName("Listing");
+        SetDiscription("TThis activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+    }
+
+    public void Run()
+    {
+        GetReady();
+        TimeSpan lenghtTime = TimeSpan.FromSeconds(GetDuration());
+
+        Random randomGenerator = new Random();
+
+        Console.WriteLine("List as many responses you can to the following prompt:");
+
+        DateTime startTime = DateTime.Now;
+
+        Console.WriteLine(_reflectivePrompts[randomGenerator.Next(0, _reflectivePrompts.Length)]);
+
+        int answerCount = 0;
+        while (DateTime.Now - startTime < lenghtTime)
+        {
+            string answer = Console.ReadLine();
+
+            if (answer.Trim().Length > 0)
+            {
+                answerCount++;
+            }
+        }
+
+        Console.WriteLine($"You listed {answerCount} items!");
+
+        EndActvity();
+    }
+}
